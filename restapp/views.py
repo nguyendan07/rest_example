c9050b92be7e146206c20e3041d603ce36ac4051
dannv6 <dannv6@fpt.com.vn>
2019-06-13 17:37:22 +0700

    Add serializers.py. Add UserList and UserDetail class. Update urls

--- restapp/views.py
+++ restapp/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import Http404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class UserList(APIView):
+    """ List all users, or create a new user. """
+
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializers = UserSerializer(users, many=True)
+        return Response(serializers.data)
+    
+    def post(self, request, format=None):
+        serializers = UserSerializer(data=request.POST)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetail(APIView):
+    """ Retrieve, update or delete a user instance. """
+    
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.POST)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
